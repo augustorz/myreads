dcbbb47df1cabd30bf3aaadda97ad186b9d09637
Augusto Zanoni <augusto.zanoni@sabia.com.br>
2019-02-17 21:24:25 -0300

    refactor: adds PropTypes to BookSelect
    Add prop type checking to BookSelect

---
+++ src/components/BookSelect/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BookSelect = ({ book, onChangeBookShelf }) => (
+  <div className="book-shelf-changer">
+    <select value={book.shelf} onChange={(event) => { onChangeBookShelf({book, newShelf: event.target.value}); }}>
+      <option value="move" disabled>Move to...</option>
+      <option value="currentlyReading">Currently Reading</option>
+      <option value="wantToRead">Want to Read</option>
+      <option value="read">Read</option>
+      <option value="none">None</option>
+    </select>
+  </div>
+)
+
+BookSelect.propTypes = {
+  book: PropTypes.object.isRequired,
+  onChangeBookShelf: PropTypes.func.isRequired
+}
+
+export default BookSelect;
