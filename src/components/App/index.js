d4dc636f2aca633ce5904b0c779eab9092696f28
Augusto Zanoni <augusto.zanoni@sabia.com.br>
2019-02-18 22:18:14 -0300

    feature: adds react router to project
    Add React Router to project

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,49 +1,15 @@
-import React from 'react'
-import './index.css'
+import React from 'react';
+import { Route } from 'react-router-dom';
+import './index.css';
 
-import BookList from '../BookList'
+import BookList from '../../pages/BookList';
+import BookSearch from '../../pages/BookSearch';
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+const App = () => (
+  <div className="app">
+    <Route exact path="/" component={BookList} />
+    <Route path="/search" component={BookSearch} />
+  </div>
+);
 
-  render() {
-    return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <BookList />
-        )}
-      </div>
-    )
-  }
-}
-
-export default BooksApp
+export default App;
