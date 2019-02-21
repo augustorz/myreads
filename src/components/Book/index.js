a5c8b9a0c6eab700e72b99c2a2f6db351b14c6e4
Augusto Zanoni <augusto.zanoni@sabia.com.br>
2019-02-21 08:55:20 -0300

    fix: add default to imageLinks

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -7,7 +7,7 @@ const Book = ({ book, onChangeBookShelf }) => (
   <li>
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }} />
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks && book.imageLinks.thumbnail}")` }} />
         <BookSelect book={book} onChangeBookShelf={onChangeBookShelf} />
       </div>
       <div className="book-title">{book.title}</div>
