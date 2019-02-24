6a86eb2e2239df01786c457c7fd5ecfcf3fed991
Augusto Zanoni <augusto.zanoni@sabia.com.br>
2019-02-24 13:48:37 -0300

    refactor: separate constants in isolated file

---
+++ src/components/BookSelect/constants.js
@@ -0,0 +1,10 @@
+export const SHELVES = [
+  { value: 'currentlyReading', text: 'Currently Reading' },
+  { value: 'wantToRead', text: 'Want to Read' },
+  { value: 'read', text: 'Read' },
+  { value: 'none', text: 'None' },
+];
+
+export default {
+  SHELVES,
+};
