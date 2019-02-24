18338894ebfc09daefd9c3e0b85d1f87d2cfd0a2
Augusto Zanoni <augusto.zanoni@sabia.com.br>
2019-02-24 13:48:16 -0300

    refactor: add propTypes and defaultProps
    Add prop type checking and default props to book

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -4,23 +4,43 @@ import PropTypes from 'prop-types';
 import BookSelect from '../BookSelect';
 
 const Book = ({ book, onChangeBookShelf }) => (
-  <li>
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks && book.imageLinks.thumbnail}")` }} />
-        <BookSelect book={book} onChangeBookShelf={onChangeBookShelf} />
-      </div>
-      <div className="book-title">{book.title}</div>
-      {book.authors && book.authors.map(author => (
-        <div className="book-authors" key={author}>{author}</div>
-      ))}
+  <div className="book">
+    <div className="book-top">
+      <div
+        className="book-cover"
+        style={{
+          width: 128,
+          height: 193,
+          backgroundImage: `url("${book.imageLinks && book.imageLinks.thumbnail}")`,
+        }}
+      />
+      <BookSelect book={book} onChangeBookShelf={onChangeBookShelf} />
     </div>
-  </li>
+    <div className="book-title">{book.title}</div>
+    {book.authors && book.authors.map(author => (
+      <div className="book-authors" key={author}>{author}</div>
+    ))}
+  </div>
 );
 
 Book.propTypes = {
-  book: PropTypes.instanceOf(Object).isRequired,
+  book: PropTypes.shape({
+    imageLinks: PropTypes.shape({
+      thumbnail: PropTypes.string,
+    }),
+    title: PropTypes.string.isRequired,
+    authors: PropTypes.instanceOf(Array),
+  }),
   onChangeBookShelf: PropTypes.func.isRequired,
+};
+
+Book.defaultProps = {
+  book: {
+    imageLinks: {
+      thumbnail: '',
+    },
+    authors: [],
+  },
 };
 
 export default Book;
