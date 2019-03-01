953fceba2c0b31bf764c246b4ba69e33f18d7bf3
Augusto Zanoni <augusto.zanoni@sabia.com.br>
2019-03-01 17:54:15 -0300

    feature: add tests

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { shallow } from 'enzyme';
+import { waitForElement } from 'enzyme-async-helpers';
+import generateBooks from '../../mocks/book';
 import App from '.';
+import BookList from '../../pages/BookList';
 
 describe('App', () => {
   let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>,
-    );
-  });
 
-  it('should match snapshot', () => {
-    expect(wrapper.html()).toMatchSnapshot();
+  beforeEach(() => {
+    fetch.resetMocks();
   });
 
   describe('componentDidMount', () => {
-    
+    let readBooks;
+    let wantToReadBooks;
+    let currentlyReadingBooks;
+
+    beforeEach(async (done) => {
+      readBooks = generateBooks(1, 'read');
+      wantToReadBooks = generateBooks(1, 'wantToRead');
+      currentlyReadingBooks = generateBooks(1, 'currentlyReading');
+
+      fetch.mockResponse(JSON.stringify({
+        books: [
+          ...readBooks,
+          ...wantToReadBooks,
+          ...currentlyReadingBooks,
+        ],
+      }));
+
+      wrapper = shallow(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>,
+      );
+      await waitForElement(wrapper, BookList);
+      done();
+    });
+
+    it('should match snapshot', () => {
+      expect(wrapper.html()).toMatchSnapshot();
+    });
   });
 });
