86a5fc86a82b9b0aede3d4e079f9b744a5d0b95e
Augusto Roberto Zanoni <gurzanoni@gmail.com>
2019-03-06 20:12:43 -0300

    fix: add import of css file

--- src/pages/BookSearch/index.js
+++ src/pages/BookSearch/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as BooksAPI from '../../api/BooksAPI';
+import './index.css';
 
 import BooksGrid from '../../components/BooksGrid';
 
