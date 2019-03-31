272435c8f402f00ffe88f526b70039a4720184c4
augustorz <gurzanoni@gmail.com>
2019-03-31 15:50:47 -0300

    fix: removed api calls from BookList

--- src/pages/BookList/index.js
+++ src/pages/BookList/index.js
@@ -1,68 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { instanceOf, shape, func } from 'prop-types';
 import { SHELVES } from './constants';
 import './index.css';
-import * as BooksAPI from '../../api/BooksAPI';
 
 import BookShelf from '../../components/BookShelf';
 
-class BookList extends Component {
-  componentDidMount = () => {
-    const { onMount } = this.props;
-
-    onMount();
-    this.loadBooks();
-  }
-
-  loadBooks = async () => {
-    const { onBooksLoad } = this.props;
-
-    try {
-      const books = await BooksAPI.getAll();
-      onBooksLoad(books);
-    } catch (error) {
-      throw (error);
-    }
-  }
-
-  render() {
-    const { shelves, onChangeBookShelf } = this.props;
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          {SHELVES.map(({ title, key }) => (
-            <BookShelf
-              key={key}
-              title={title}
-              books={shelves[key]}
-              onChangeBookShelf={onChangeBookShelf}
-            />
-          ))}
-        </div>
-        <div className="open-search">
-          <Link to="/search">
-            <button type="button">Add a book</button>
-          </Link>
-        </div>
-      </div>
-    );
-  }
-}
+const BookList = ({ shelves, onChangeBookShelf }) => (
+  <div className="list-books">
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+    <div className="list-books-content">
+      {SHELVES.map(({ title, key }) => (
+        <BookShelf
+          key={key}
+          title={title}
+          books={shelves[key]}
+          onChangeBookShelf={onChangeBookShelf}
+        />
+      ))}
+    </div>
+    <div className="open-search">
+      <Link to="/search">
+        <button type="button">Add a book</button>
+      </Link>
+    </div>
+  </div>
+);
 
 BookList.propTypes = {
-  shelves: PropTypes.shape({
-    currentlyReading: PropTypes.instanceOf(Array),
-    wantToRead: PropTypes.instanceOf(Array),
-    read: PropTypes.instanceOf(Array),
+  shelves: shape({
+    currentlyReading: instanceOf(Array),
+    wantToRead: instanceOf(Array),
+    read: instanceOf(Array),
   }).isRequired,
-  onChangeBookShelf: PropTypes.func.isRequired,
-  onBooksLoad: PropTypes.func.isRequired,
-  onMount: PropTypes.func.isRequired,
+  onChangeBookShelf: func.isRequired,
 };
 
 export default BookList;
--- src/pages/BookList/index.test.js
+++ src/pages/BookList/index.test.js
@@ -7,21 +7,11 @@ import BookList from '.';
 
 describe('BookList', () => {
   let routerWrapper;
-  let wrapper;
-  let books;
 
   beforeEach(() => {
-    fetch.resetMocks();
-
     const currentlyReading = createBooks({ quantity: 2, shelf: 'currentlyReading' });
     const wantToRead = createBooks({ quantity: 3, shelf: 'wantToRead' });
     const read = createBooks({ quantity: 1, shelf: 'read' });
-
-    books = [
-      ...currentlyReading,
-      ...wantToRead,
-      ...read,
-    ];
 
     const shelves = {
       currentlyReading: [...currentlyReading],
@@ -29,59 +19,17 @@ describe('BookList', () => {
       read: [...read],
     };
 
-    fetch.mockResponse(JSON.stringify({
-      books,
-    }));
-
     routerWrapper = mount(
       <MemoryRouter>
         <BookList
           shelves={shelves}
-          onMount={jest.fn()}
-          onBooksLoad={jest.fn()}
           onChangeBookShelf={jest.fn()}
         />
       </MemoryRouter>,
     );
-
-    wrapper = routerWrapper.find(BookList);
   });
 
   it('should match snapshot', () => {
     expect(routerWrapper.html()).toMatchSnapshot();
-  });
-
-  describe('loadBooks', () => {
-    describe('when async call has no errors', () => {
-      beforeEach(async (done) => {
-        await wrapper.instance().loadBooks();
-        done();
-      });
-
-      it('should call onBooksLoad with books', () => {
-        expect(wrapper.props().onBooksLoad).toBeCalledWith(books);
-      });
-    });
-
-    describe('when async calls has errors', () => {
-      beforeEach(() => {
-        fetch.mockReject(new Error('Network Error'));
-      });
-
-      it('should throw error', async (done) => {
-        try {
-          await wrapper.instance().loadBooks();
-        } catch (e) {
-          expect(e.message).toEqual('Network Error');
-        }
-        done();
-      });
-    });
-  });
-
-  describe('componentDidMount', () => {
-    it('should call onMount', () => {
-      expect(wrapper.props().onMount).toBeCalled();
-    });
   });
 });
